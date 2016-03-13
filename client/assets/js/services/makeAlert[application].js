3a41c58ef7afd26df1c853c6176337c6b0a9e1d8
zinkoff <tosti@mikbook.local>
2016-03-13 13:06:52 +0100

    update1

--- client/assets/js/services/makeAlert[application].js
+++ client/assets/js/services/makeAlert[application].js
@@ -1,4 +1,4 @@
-angular.module('application').factory('alert', ['$window', function(win) {
+angular.module('application').factory('makeAlert', ['$window', function(win) {
 
    var skriv = [];
    return function(skriv) {
