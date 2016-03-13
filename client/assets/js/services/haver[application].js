7fa0f1251f8b176cc83b6e973c01eaf114b1ed67
zinkoff <tosti@mikbook.local>
2016-03-13 19:26:38 +0100

    update

--- client/assets/js/services/haver[application].js
+++
@@ -1,6 +0,0 @@
-angular.module('application').factory('haver', ['$resource',
-	function($resource){
-    	return $resource('http://mikbook.local:60014/test/api/lists/', {}, {
-      		query: {method:'GET', params:{}, isArray:true}
-    		});
-  	}]);
